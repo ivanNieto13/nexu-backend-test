ea824ea21c573cbd7018684b9677e5f479ce9fff
Ivan Nieto <ivannieto59@aragon.unam.mx>
2024-06-22 15:36:46 -0500

    feat: add validate average price middleware on update

--- src/routes/model.router.ts
+++ src/routes/model.router.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import { updateAveragePriceController } from '../controllers/model.controller';
 import { ModelServiceInterface } from '../services/model.service';
+import { validateAveragePriceMiddleware } from '../middleware/validate-average-price.middleware';
 
 const router = Router();
 
 export const modelRouter = (modelService: ModelServiceInterface) => {
   router.route('/:id')
-    .put(updateAveragePriceController(modelService));
+    .put(
+      validateAveragePriceMiddleware,
+      updateAveragePriceController(modelService),
+    );
 
   return router;
 }
