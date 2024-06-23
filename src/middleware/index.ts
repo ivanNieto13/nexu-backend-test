365b0d07f5b041299424e31543276623ffdac1ed
Ivan Nieto <ivannieto59@aragon.unam.mx>
2024-06-23 14:13:07 -0500

    feat: add validate brand name exists middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,3 +2,4 @@ export * from './validate-average-price.middleware';
 export * from './validate-model-id-exists.middleware';
 export * from './validate-models-filters.middleware';
 export * from './validate-brand-id-exists.middleware';
+export * from './validate-brand-name-exists.middleware';
---
+++ src/middleware/validate-brand-name-exists.middleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+import { BrandServiceInterface } from '../services/brand.service';
+
+export const validateBrandNameExistsMiddleware = (brandService: BrandServiceInterface) => {
+  return (async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.body.name || String(req.body.name).trimEnd().trimStart() === '') {
+      res.status(400).json({ message: 'you must provide a name!' });
+
+      return;
+    }
+
+    const name = String(req.body.name).trimStart().trimEnd().toLowerCase();
+    const model = await brandService.getBrandByName(name);
+
+    if (model.length) {
+      res.status(409).json({ message: `brand with name ${req.body.name} already exists` });
+
+      return;
+    }
+
+    next();
+  })
+}
