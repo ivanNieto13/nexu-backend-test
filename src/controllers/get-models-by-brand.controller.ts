2787c914c72254b8d86969179521d0c6d4086b14
Ivan Nieto <ivannieto59@aragon.unam.mx>
2024-06-22 22:20:50 -0500

    feat: add get models by brand id controller

---
+++ src/controllers/get-models-by-brand.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { ModelServiceInterface } from '../services/model.service';
+import { getModelsFiltersDto } from '../dto/get-models-filters.dto';
+
+export const getModelsByBrandController = (modelService: ModelServiceInterface) => {
+  return (async (req: Request, res: Response) => {
+    try {
+      const input: Partial<getModelsFiltersDto> = {
+        brand_id: req.params.id,
+      };
+      const models = await modelService.getModels(input as getModelsFiltersDto);
+      res.status(200);
+      res.json(models);
+    } catch (err) {
+      res.status(500);
+      res.json({ message: err });
+    }
+  });
+}
