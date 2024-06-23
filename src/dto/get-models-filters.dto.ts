5fd323585d293e224ed994c7a8fae10792c0f21a
Ivan Nieto <ivannieto59@aragon.unam.mx>
2024-06-22 20:42:02 -0500

    feat: add get model filters dto

---
+++ src/dto/get-models-filters.dto.ts
@@ -0,0 +1,6 @@
+interface ParsedQs { }
+
+export interface getModelsFiltersDto {
+  greater: string | ParsedQs | string[] | ParsedQs[] | undefined;
+  lower: string | ParsedQs | string[] | ParsedQs[] | undefined;
+}
