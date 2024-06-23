a3ab757f6b8acdd788e07362d2a3ac93ed635c1c
Ivan Nieto <ivannieto59@aragon.unam.mx>
2024-06-22 19:09:32 -0500

    feat: add get model by id service method

--- src/services/model.service.ts
+++ src/services/model.service.ts
@@ -1,6 +1,7 @@
 import { ModelRepositoryInterface } from '../repositories/model.repository';
 
 export interface ModelServiceInterface {
+  getModelById(value: number): Promise<ModelEntityInterface>;
   updateAveragePrice(value: UpdateAveragePriceDto): Promise<ModelEntityInterface>;
 }
 
@@ -9,6 +10,10 @@ export class ModelService implements ModelServiceInterface {
 
   constructor(modelRepository: ModelRepositoryInterface) {
     this.modelRepository = modelRepository;
+  }
+
+  public getModelById(value: number): Promise<ModelEntityInterface> {
+    return this.modelRepository.getModelById(value);
   }
 
   public updateAveragePrice({ id, averagePrice }: UpdateAveragePriceDto): Promise<ModelEntityInterface> {
