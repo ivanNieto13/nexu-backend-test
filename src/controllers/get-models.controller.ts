08215f05fa3c0bbe26721826e20ffb645a660c82
Ivan Nieto <ivannieto59@aragon.unam.mx>
2024-06-22 20:43:07 -0500

    feat: set filters on get models controller

--- src/controllers/get-models.controller.ts
+++ src/controllers/get-models.controller.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from 'express';
 import { ModelServiceInterface } from '../services/model.service';
+import { getModelsFiltersDto } from '../dto/get-models-filters.dto';
 
 export const getModelsController = (modelService: ModelServiceInterface) => {
   return (async (req: Request, res: Response) => {
     try {
-      const models = await modelService.getModels();
+      const filters: getModelsFiltersDto = {
+        greater: req.query.greater,
+        lower: req.query.lower,
+      };
+      const models = await modelService.getModels(filters);
       res.status(200);
       res.json(models);
     } catch (err) {
