ed369f12211c812a65085509d6fb4cb0e242c6a6
Ivan Nieto <ivannieto59@aragon.unam.mx>
2024-06-22 21:58:38 -0500

    feat: add brand_id to get models filter dto

--- src/dto/get-models-filters.dto.ts
+++ src/dto/get-models-filters.dto.ts
@@ -3,4 +3,5 @@ interface ParsedQs { }
 export interface getModelsFiltersDto {
   greater: string | ParsedQs | string[] | ParsedQs[] | undefined;
   lower: string | ParsedQs | string[] | ParsedQs[] | undefined;
+  brand_id: string;
 }
