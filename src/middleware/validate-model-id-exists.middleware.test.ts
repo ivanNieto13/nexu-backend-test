72830e0641a3bf3512579edce10d4f2d7c17e069
Ivan Nieto <ivannieto59@aragon.unam.mx>
2024-06-24 12:39:18 -0500

    feat: add validate model exists middleware test cases

---
+++ src/middleware/validate-model-id-exists.middleware.test.ts
@@ -0,0 +1,72 @@
+import { validateModelIdExistsMiddleware } from './validate-model-id-exists.middleware';
+import { ModelServiceInterface } from '../services/model.service';
+import { NextFunction, Request, Response } from 'express';
+
+describe('validateModelIdExistsMiddleware', () => {
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let next: NextFunction;
+  let modelService: Partial<ModelServiceInterface>;
+
+  beforeEach(() => {
+    req = {};
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    next = jest.fn();
+    modelService = {
+      getModelById: jest.fn()
+    };
+  });
+
+  test('should return 400 if id is not provided', async () => {
+    req.params = {};
+
+    const middleware = validateModelIdExistsMiddleware(modelService as ModelServiceInterface);
+
+    await middleware(req as Request, res as Response, next);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({ message: 'you must provide an id!' });
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  test('should return 400 if id is invalid', async () => {
+    req.params = { id: '-1' };
+
+    const middleware = validateModelIdExistsMiddleware(modelService as ModelServiceInterface);
+
+    await middleware(req as Request, res as Response, next);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({ message: 'you must provide a valid id!' });
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  test('should return 404 if model with id is not found', async () => {
+    req.params = { id: '1' };
+    (modelService.getModelById as jest.Mock).mockResolvedValue({});
+
+    const middleware = validateModelIdExistsMiddleware(modelService as ModelServiceInterface);
+
+    await middleware(req as Request, res as Response, next);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({ message: 'model with id 1 not found' });
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  test('should call next if model with id is found', async () => {
+    req.params = { id: '1' };
+    (modelService.getModelById as jest.Mock).mockResolvedValue({ id: 1 });
+
+    const middleware = validateModelIdExistsMiddleware(modelService as ModelServiceInterface);
+
+    await middleware(req as Request, res as Response, next);
+
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.json).not.toHaveBeenCalled();
+    expect(next).toHaveBeenCalled();
+  });
+});
